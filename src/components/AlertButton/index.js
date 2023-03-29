98ba93e0df10bac49242ee4b967c0963bf924e3f
Flávia Figueredo <flaviagfigueredo@gmail.com>
2023-03-29 12:05:05 -0300

    função de alerta passada para dentro da arrow function do onClick

--- src/components/AlertButton/index.js
+++ src/components/AlertButton/index.js
@@ -1,11 +1,7 @@
 import "./index.css"
 
-const showLabel = ({label}) => {
-    alert(`A label desse botão é: ${label}`)
-}
-
 const AlertButton = ({label}) => {
-    return <button className="btn" onClick={() => showLabel({label})}>{label}</button>
+    return <button className="btn" onClick={() => alert(`A label desse botão é: ${label}`)}>{label}</button>
 }
 
 export default AlertButton
